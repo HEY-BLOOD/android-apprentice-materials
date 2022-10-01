9ff71b275ad68269e6e2689e11a2c339e249ff4a
hwangblood <hwangblood@qq.com>
2022-10-02 00:33:44 +0800

    Chapter 3: Hooking up Views

--- Timefighter/app/src/main/java/com/hwangblood/timefighter/MainActivity.kt
+++ Timefighter/app/src/main/java/com/hwangblood/timefighter/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.hwangblood.timefighter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // connect views to variables
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+        tapMeButton.setOnClickListener { incrementScore() }
+    }
+
+    private fun incrementScore() {
+        // increment score logic
+        score++
+        val newScore = "Your Score: $score"
+        gameScoreTextView.text = newScore
+    }
+
+    private fun resetGame() {
+        // reset game logic
+    }
+
+    private fun startGame() {
+        // start game logic
+    }
+
+    private fun endGame() {
+        // end game logic
     }
 }
