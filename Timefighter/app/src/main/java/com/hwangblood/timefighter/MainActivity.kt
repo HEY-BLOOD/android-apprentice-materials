5659a98a5617927284bd983bafb27922e6a559d2
hwangblood <hwangblood@qq.com>
2022-10-11 10:11:36 +0800

    Chapter 5: Adding a Dialog

--- Timefighter/app/src/main/java/com/hwangblood/timefighter/MainActivity.kt
+++ Timefighter/app/src/main/java/com/hwangblood/timefighter/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.hwangblood.timefighter
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -56,6 +59,32 @@ class MainActivity : AppCompatActivity() {
             resetGame()
         }
         Log.d(TAG, "onCreate called. Score is: $score")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about_item) {
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(
+            R.string.about_title,
+            BuildConfig.VERSION_NAME
+        )
+        val dialogMessage = getString(R.string.about_message)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 
     private fun restoreGame() {
