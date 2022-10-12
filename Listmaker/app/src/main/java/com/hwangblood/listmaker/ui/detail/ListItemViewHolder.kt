e88dc7dbfcac36258628aebc2db48ff5edb8d88e
hwangblood <hwangblood@qq.com>
2022-10-12 17:01:45 +0800

    Chapter 10: Coding the RecyclerView

---
+++ Listmaker/app/src/main/java/com/hwangblood/listmaker/ui/detail/ListItemViewHolder.kt
@@ -0,0 +1,7 @@
+package com.hwangblood.listmaker.ui.detail
+
+import androidx.recyclerview.widget.RecyclerView
+
+// TODO Create list_itemview_holder.xml layout
+class ListItemViewHolder(private val binding: ListItemViewHolderBinding) :
+    RecyclerView.ViewHolder(binding.root)
---
+++ Listmaker/app/src/main/java/com/hwangblood/listmaker/ui/detail/ListItemsRecyclerViewAdapter.kt
@@ -0,0 +1,7 @@
+package com.hwangblood.listmaker.ui.detail
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hwangblood.listmaker.TaskList
+
+class ListItemsRecyclerViewAdapter(var list: TaskList) : RecyclerView.Adapter<ListItemViewHolder>()
